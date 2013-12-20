db59a8325aa5fe4f9ff886b5cd47b0528a2414af
Agus Kurniawan <agusk2007@gmail.com>
2013-12-20 07:33:27 +0100

    updated

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 
 var random = require('./RandomPassword.js');
 
-for(var i=0;i<10;i++) {
+for(var i=0;i<5;i++) {
     console.log(random.generatePassword(10))
 }
 
+for(var i=0;i<5;i++) {
+    console.log(random.generatePassword(15))
+}
+
+for(var i=0;i<5;i++) {
+    console.log(random.generatePassword(20))
+}
 
